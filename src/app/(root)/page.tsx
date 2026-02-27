e46964534fc09295ecb22a914ac607af227f541d
Rohit Sharma <rohits8731@gmail.com>
2026-02-28 00:04:23 +0530

    02: Navbar

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = async () => {
+    return (
+        <main className="wrapper container">
+            Home
+        </main>
+    )
+}
+
+export default Home
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Navbar } from "@/components/Navbar";
 
 const ibmPlexSerif = IBM_Plex_Serif({
     variable: "--font-ibm-plex-serif",
@@ -31,6 +32,7 @@ export default function RootLayout({
             <body
             className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
             >
+                <Navbar />
                 {children}
                 <Toaster />
             </body>
